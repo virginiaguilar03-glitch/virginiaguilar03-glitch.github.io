a7fd624f7b97891c534040cb8552f7ee1ce5d8a5
virginiaguilar03-glitch <virginiaguilar03@gmail.com>
2026-07-12 17:01:02 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "COLE_AQUI_SUA_URL";
+const SUPABASE_URL = "https://oceikxdqjgkewwziaabj.supabase.co/rest/v1/";
 
-const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_CHAVE_PUBLICA";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jZWlreGRxamdrZXd3emlhYWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzQyODIsImV4cCI6MjA5OTQ1MDI4Mn0.8hEgQZupDGoNP2I14JbkgelT6TaeRWq7SwDxRgQ7UNw";
 
 
 const supabaseClient = supabase.createClient(
