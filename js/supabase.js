fa4245db259db67197f5b1961aec98ab45cf8e5d
virginiaguilar03-glitch <virginiaguilar03@gmail.com>
2026-08-13 15:15:16 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,6 @@
-const SUPABASE_URL = "https://oceikxdqjgkewwziaabj.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://lwgdryiyksopjsyqfowz.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jZWlreGRxamdrZXd3emlhYWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzQyODIsImV4cCI6MjA5OTQ1MDI4Mn0.8hEgQZupDGoNP2I14JbkgelT6TaeRWq7SwDxRgQ7UNw";
-
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3Z2RyeWl5a3NvcGpzeXFmb3d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MTE0NzAsImV4cCI6MjEwMjE4NzQ3MH0.7oINUZaLEOGGwj6GXB0Sb89BwYK4i9_itV_M4f70Rws";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
