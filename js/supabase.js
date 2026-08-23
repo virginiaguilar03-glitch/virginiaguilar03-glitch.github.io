4d70abbfbaf6b27d132d63fdd93f788d249d1f4e
virginiaguilar03-glitch <virginiaguilar03@gmail.com>
2026-08-22 22:52:00 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,31 @@
+// ============================================================
+// SUPABASE - VAIDTÁXI
+// ============================================================
+
+// Configuração do projeto Supabase
+
+
 const SUPABASE_URL = "https://lwgdryiyksopjsyqfowz.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3Z2RyeWl5a3NvcGpzeXFmb3d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MTE0NzAsImV4cCI6MjEwMjE4NzQ3MH0.7oINUZaLEOGGwj6GXB0Sb89BwYK4i9_itV_M4f70Rws";
 
-const supabaseClient = supabase.createClient(
+
+
+
+/*
+ * Cria o cliente Supabase.
+ *
+ * Este objeto será utilizado por:
+ *
+ * cadastro.js
+ * login.js
+ * auth.js
+ * corrida.js
+ * motoristas.js
+ * pagamentos.js
+ */
+
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
